5f549801ca1fb6bccdfab021409323b0029e4906
Celso Zacarias <celso.zacarias@estudante.ufjf.br>
2023-12-04 00:39:53 -0300

    Implementado restante do código main.js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 //main.js
 
+import { getLista } from "./lista.js";
+
 const olItens = document.querySelector("#itens");
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#Adicionar");
 const btnLimpar = document.querySelector("#limpar");
+
+atualizarLista();
+
+function atualizarLista(){
+    olItens.innerHTML = "";
+    let lista = getList();
+    for (let i = 0; i < lista.length; i++){
+        const li = document.createElement('li');
+        li.textContent = lista[i];
+        olItens.appendChild(li);
+    }
+}
