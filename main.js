659a7ca7680c3fafefa3246d9a4146fa2689d413
Celso Zacarias <celso.zacarias@estudante.ufjf.br>
2023-12-04 23:54:55 -0300

    Correção de digitação no id buscado no arquivo js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { getLista, limpaLista, adicionaNaLista, removeDaLista } from "./lista.js
 
 const olItens = document.querySelector("#itens");
 const pEntrada = document.querySelector('#entrada');
-const btnAdicionar = document.querySelector("#Adicionar");
+const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 
 btnLimpar.addEventListener('click', limparItensDeLista);
